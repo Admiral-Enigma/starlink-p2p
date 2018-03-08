1f66dd40cbd47fdea07266dde1dcbedc0461e593
Alexander <MrRandomgamerdkHD@gmail.com>
2018-03-08 19:55:28 +0100

    Added blink to client

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 var JSONTCPSOCKET = require('json-tcp-socket');
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
 
 var JSONTCPSOCKET = new JSONTCPSOCKET({tls: false});
 var socket = new JSONTCPSOCKET.Socket();
@@ -7,8 +9,15 @@ socket.on('connect', function () {
   console.log('Connected');
   socket.on('data', function (data) {
     if (data.blink) {
-      console.log('BLINK');
+      blinkLED()
     }
   })
 })
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1);
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
 socket.connect(1337, '192.168.123.88');
